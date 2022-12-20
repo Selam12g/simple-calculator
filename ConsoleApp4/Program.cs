1fb5508047695e7d8bb2a5732d05772fe1f4640c
Dell <Dell@DESKTOP-9319AP6>
2022-12-20 22:28:28 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.ExceptionServices;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("input the first number; ");
+int firstvalue = int.Parse(Console.ReadLine());
+Console.WriteLine("input the second number; ");
+int secondvalue = int.Parse(Console.ReadLine());
+Console.WriteLine("press 1. for addition   2. for subtraction    3. for multiplication    4. for divvision");
+int operation = int.Parse(Console.ReadLine());
+if(operation == 1)
+{
+    int result = firstvalue + secondvalue;
+    Console.WriteLine("the result is;" + result);
+}
+else if (operation == 2)
+{
+    int result = firstvalue - secondvalue;
+    Console.WriteLine("the result is; " + result);
+}
+else if (operation == 3)
+{
+    int result = firstvalue * secondvalue;
+    Console.WriteLine("the result is; " + result);
+}
+else if (operation == 4)
+{
+    int result = firstvalue / secondvalue;
+    Console.WriteLine("the result is; " + result);
+}
+else
+{
+    Console.WriteLine("inappropriate input");   
+}
